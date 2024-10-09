66fa0a336a3f7ba3078496ca40e281e9a3ad510e
tony-stark18 <sahoodiptiranjan885@gmail.com>
2024-10-09 18:29:22 +0530

    TLE_800_06_HashMap_solution

--- 800/A_Doremy_s_Paint_3.cpp
+++ 800/A_Doremy_s_Paint_3.cpp
@@ -1,37 +1,29 @@
 #include <iostream>
+#include <unordered_map>
 #include <vector>
 using namespace std;
 
 bool soln(vector<int> &arr, int n)
 {
-    int dist_1 = arr[0], dist_1_count = 1;
-    int dist_2 = -1, dist_2_count = 0;
+    unordered_map<int, int> freq;
 
-    for (int i = 1; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
-        if (arr[i] == dist_1)
-        {
-            dist_1_count++; 
-        }
-        else if (dist_2 == -1)
-        {
-            dist_2 = arr[i];
-            dist_2_count++; 
-        }
-        else if (arr[i] == dist_2)
-        {
-            dist_2_count++; 
-        }
-        else
-        {
-            return false; 
-        }
+        freq[arr[i]]++;
     }
 
-    if (dist_2_count == 0)
+    if (freq.size() > 2)
+        return false;
+
+    if (freq.size() == 1)
         return true;
 
-    return abs(dist_1_count - dist_2_count) <= 1;
+    auto it = freq.begin();
+    int count1 = it->second;
+    it++;
+    int count2 = it->second;
+
+    return abs(count1 - count2) <= 1;
 }
 
 int main()
